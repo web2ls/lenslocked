7cba1bdde324e2193dec562ed16f62559020e1d3
web2ls <web2ls@yandex.ru>
2019-06-13 15:28:52 +0300

    added view for SignUp page

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 var homeView *views.View
 var contactView *views.View
 var faqView *views.View
+var signupView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -26,6 +27,11 @@ func contact(w http.ResponseWriter, r *http.Request) {
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(faqView.Render(w, nil))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
@@ -44,6 +50,7 @@ func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("secondary", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	if err != nil {
 		panic(err)
@@ -53,6 +60,7 @@ func main() {
 	router.HandleFunc("/", home)
 	router.HandleFunc("/contact", contact)
 	router.HandleFunc("/faq", faq)
+	router.HandleFunc("/signup", signup)
 
 	var h http.Handler = http.HandlerFunc(notFound)
 	router.NotFoundHandler = h
