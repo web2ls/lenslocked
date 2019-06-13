099d8521fd266c44e64c419d612e2d95b8ae82cc
web2ls <web2ls@yandex.ru>
2019-06-13 16:14:53 +0300

    added Users controller

---
+++ controllers/users.go
@@ -0,0 +1,20 @@
+package controllers
+
+import "lenslocked/views"
+import "net/http"
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"lenslocked/controllers"
 	"lenslocked/views"
 
 	"github.com/gorilla/mux"
@@ -12,7 +13,6 @@ import (
 var homeView *views.View
 var contactView *views.View
 var faqView *views.View
-var signupView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -27,11 +27,6 @@ func contact(w http.ResponseWriter, r *http.Request) {
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(faqView.Render(w, nil))
-}
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
@@ -50,7 +45,7 @@ func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("secondary", "views/faq.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersController := controllers.NewUsers()
 
 	if err != nil {
 		panic(err)
@@ -60,7 +55,7 @@ func main() {
 	router.HandleFunc("/", home)
 	router.HandleFunc("/contact", contact)
 	router.HandleFunc("/faq", faq)
-	router.HandleFunc("/signup", signup)
+	router.HandleFunc("/signup", usersController.New)
 
 	var h http.Handler = http.HandlerFunc(notFound)
 	router.NotFoundHandler = h
